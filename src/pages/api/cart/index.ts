29444fb3a9b1c712f1921122e0d904092afaaf88
Felipe <felipesullivan50@gmail.com>
2023-05-01 15:55:21 -0300

    🐞 fix: middleware
    fix runMiddleware at cart product route

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -1,4 +1,4 @@
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest } from "next/server";
 import url from "url";
 import { productMocked } from "../../../productMocked";
@@ -8,10 +8,28 @@ const { cors } = corsMiddleware();
 
 cors(["GET"]);
 
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 export default async function handler(
-  req: NextRequest,
+  req: NextRequest & NextApiRequest,
   res: NextApiResponse
 ) {
+
+  await runMiddleware(req, res, cors)
 
   const { query } = url.parse(req.url, true);
 
